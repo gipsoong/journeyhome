fde904ebb2e97c7eec6f634c746424ccdcf0c4ee
Ryan <ryangipsoong@gmail.com>
2025-06-06 01:25:35 +0800

    Import Bootstrap and React Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FcHome } from "react-icons/fc"
 import Form from './components/form'
 
 Form
@@ -12,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Mortgage Calculator</h1>
+        <h1> <FcHome />Mortgage Calculator</h1>
         <Form />
       </div>
     </>
