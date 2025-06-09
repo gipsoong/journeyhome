dc46b2db97925a68f6833d9e754b31e995127ce6
Ryan <ryangipsoong@gmail.com>
2025-06-09 22:53:40 +0800

    Finishing up main mortgage calculator

--- src/components/FormInputGroup.jsx
+++ src/components/FormInputGroup.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-function FormInputGroup({text, icon, placeholder, value}) {
+function FormInputGroup({text, icon, placeholder, value, onInput, onKeyUp, readOnly=false}) {
     return (
         <div className="input-group mb-3">
-            <span className="input-grou-text">{text}{icon}</span>
+            <span className="input-grou-text">
+                {text}
+                {icon}
+            </span>
             <input 
                 className="from-control"
                 type="number" 
                 placeholder={placeholder} 
-                value={value}>
-            </input>
+                value={value}
+                onInput={onInput}
+                onKeyUp={onKeyUp}
+                readOnly={readOnly}
+            />
         </div>
     )
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,16 +1,85 @@
-import React from "react";
-import FormInputGroup from "./FormInputGroup";
+import React, {useState} from "react";
+import FormInputGroup from "./formInputGroup";
 import { CiBadgeDollar } from "react-icons/ci";
 
 
 function Form() {
+    const [homeValue, setHomeValue] = useState("");
+    const [downPayment, setDownPayment] = useState("");
+    const [loanAmount, setLoanAmount] = useState("");
+    const [interestRate, setInterestRate] = useState("");
+    const [loanDuration, setLoanDuration] = useState("");
+    const [monthlyPayment, setMonthlyPayment] = useState(0);
+
+    function calculateLoanAmount() {
+        setLoanAmount(homeValue - downPayment);
+        return loanAmount;
+    }
+    
+    function calculateMonthlyPayment() {
+        function percentagetoDecimal(percent){
+            return percent / 12 / 100 ;
+        }
+
+        function yearsToMonths(year){
+            return year * 12;
+        }
+
+        setMonthlyPayment(
+            (percentagetoDecimal(interestRate * loanAmount)/ (1-Math.pow(1+percentagetoDecimal(interestRate), -yearsToMonths(loanDuration))))
+        );
+        
+        console.log(monthlyPayment)
+        return monthlyPayment;
+
+    }
+  
+    
     return (
         <form onSubmit={(e)=> e.preventDefault()}>
-            <FormInputGroup text="Home Value" icon={<CiBadgeDollar />}/>
+            <FormInputGroup 
+                text="Home Value" 
+                icon={<CiBadgeDollar />}
+                placeholder={"Enter the value of the flat"}
+                onKeyUp={calculateLoanAmount}
+                value={homeValue}
+                onInput={(e) => setHomeValue(e.target.value)}
+            />
+            <FormInputGroup 
+                text="Down Payment" 
+                icon={<CiBadgeDollar />}
+                placeholder={"Enter your funds"}
+                onKeyUp={calculateLoanAmount}
+                value={downPayment}
+                onInput={(e) => setDownPayment(e.target.value)}
+            />
+            <FormInputGroup 
+                text="Interest Rate %" 
+                placeholder={"Enter your interest rate"}
+                value={interestRate}
+                onInput={(e) => setInterestRate(e.target.value)}
+            />
+            <FormInputGroup 
+                text="Loan Duration (Years)" 
+                placeholder={"Enter the duration of your loan in years"}
+                value={loanDuration}
+                onInput={(e) => setLoanDuration(e.target.value)}
+            />
+            <FormInputGroup 
+                text="Loan Amount" 
+                icon={<CiBadgeDollar />}
+                placeholder={"Funds needed"}
+                readOnly={true}
+                value={loanAmount}
+            />
+
+            <h4 className="alert alert-info fw-bold">
+                Monthly payment: 
+                {parseFloat(monthlyPayment.toFixed(2))}</h4>
             <button 
                 type="submit" 
                 className="btn btn-primary btn-lg w-100 center" 
-                onClick={() => console.log("Click")}>
+                onClick={calculateMonthlyPayment}>
                     Calculate
             </button>
         </form>
